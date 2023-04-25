993b8789e2b5ef29e4063cbc10110d4898282e92
brnkgabriel <brnkgabriel@gmail.com>
2023-04-25 21:01:33 +0100

    edited astro.config.mjs for netlifyCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
       config: {
         backend: {
           name: "github",
+          repo: "brnkgabriel/astro-linktree",
           branch: "main"
         },
         collections: [
