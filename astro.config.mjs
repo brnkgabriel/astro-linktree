ad85a7f44ce3cf3e952a03b10cee28d2e9c58481
brnkgabriel <brnkgabriel@gmail.com>
2023-04-26 12:31:58 +0100

    updated to git-gateway

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
       config: {
         backend: {
           name: "git-gateway",
-          branch: "main"
+          branch: "main",
         },
         collections: [
           {
